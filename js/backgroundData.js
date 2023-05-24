a36fd126309fe5fe0d4c7f27aec93ef6cee7c569
Darklydivergent <134508531+Darklydivergent@users.noreply.github.com>
2023-05-24 12:11:40 -0400

    Add files via upload
    added first assets to test functionality

---
+++ js/backgroundData.js
@@ -0,0 +1,4 @@
+var backgroundData = [
+"1stanniv_cain_bg_1.png",
+"2ndanniv_cain_bg_1.png",
+];
---
+++ js/charData.js
@@ -0,0 +1,4 @@
+﻿var charData = {
+    "Cain (Halloween 2022)": "cain_halloween_2022",
+    "Cain (Walpurgis 2022)": "cain_warupurugis_2022",
+};
