7b6fa1b74ee21702f7f0e57e1033ca01768dca6f
Darklydivergent <134508531+Darklydivergent@users.noreply.github.com>
2023-05-24 19:22:45 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ class Viewer {
             }
 
             if (this.model) {
-                this.model.position = new PIXI.Point((width * 0.9), (height * 0.9));
+                this.model.position = new PIXI.Point((width * 1), (height * 1));
                 this.model.scale = new PIXI.Point((this.model.position.x * 0.1), (this.model.position.x * 0.09));
                 this.model.masks.resize(this.app.view.width, this.app.view.height);
             }
