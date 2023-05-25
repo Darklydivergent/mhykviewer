360067b04197b453808a8d68cbe18c3f20957b73
Darklydivergent <134508531+Darklydivergent@users.noreply.github.com>
2023-05-24 22:09:45 -0400

    Update charData.js

--- js/charData.js
+++ js/charData.js
@@ -1,4 +1,5 @@
 ﻿var charData = {
     "Cain (Halloween 2022)": "cain_halloween_2022",
     "Cain (Walpurgis 2022)": "cain_warupurugis_2022",
+    "Bradley (Valentines 2021)": "bradley_valentine_2021",
 };
